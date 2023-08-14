523e7a00b074fa2b917daa6e07b972ea5bb1ea1a
Azadeh Galidari <agalidari@gmail.com>
2023-08-14 16:11:52 +0200

    parsing request bodies

--- app.js
+++ app.js
@@ -1,32 +1,37 @@
 const http = require('http');
-const PORT = 3000;
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
     const url = req.url;
     const method = req.method;
-
     if (url === '/') {
-        res.write('<html>')
-        res.write('<head><title>Enter message</title></head>')
-        res.write('<body><form method="POST" action="message"><input type="text" name=""message><button type="submit">Send</button></form></body>')
-        res.write('</html>')
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title><head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
         return res.end();
     }
-    //process.exit()
     if (url === '/message' && method === 'POST') {
-        fs.writeFileSync('message.txt', 'DUMMY');
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt', message);
+        });
         res.statusCode = 302;
         res.setHeader('Location', '/');
         return res.end();
     }
-    res.setHeader('Content-Type', 'text/html')
-    res.write('<html>')
-    res.write('<head><title>my first page</title></head>')
-    res.write('<body><h1>Hello from my node.js server!</h1></body>')
-    res.write('</head>')
-    res.write('</html>')
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title><head>');
+    res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+    res.write('</html>');
+    res.end();
 });
 
-server.listen(PORT)
+server.listen(3000);
