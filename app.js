50de8e1094207b5eb3318b3ee7e53388426bbeaa
Azadeh Galidari <agalidari@gmail.com>
2023-08-14 15:43:28 +0200

    redirecting requests

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const PORT = 3000;
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method, req.headers);
@@ -10,16 +11,22 @@ const server = http.createServer((req, res) => {
         res.write('<html>')
         res.write('<head><title>Enter message</title></head>')
         res.write('<body><form method="POST" action="message"><input type="text" name=""message><button type="submit">Send</button></form></body>')
-        res.write('</head>')
+        res.write('</html>')
         return res.end();
     }
     //process.exit()
-    if (url === '/message' && method === 'POST')
+    if (url === '/message' && method === 'POST') {
+        fs.writeFileSync('message.txt', 'DUMMY');
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
     res.setHeader('Content-Type', 'text/html')
     res.write('<html>')
     res.write('<head><title>my first page</title></head>')
     res.write('<body><h1>Hello from my node.js server!</h1></body>')
     res.write('</head>')
+    res.write('</html>')
 });
 
 server.listen(PORT)
