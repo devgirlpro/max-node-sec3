47571c9d172b18cd6bb3492e65def29a6d5f73e6
Azadeh Galidari <agalidari@gmail.com>
2023-08-14 15:26:01 +0200

    redirecting requests

--- app.js
+++ app.js
@@ -3,7 +3,18 @@ const PORT = 3000;
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method, req.headers);
+    const url = req.url;
+    const method = req.method;
+
+    if (url === '/') {
+        res.write('<html>')
+        res.write('<head><title>Enter message</title></head>')
+        res.write('<body><form method="POST" action="message"><input type="text" name=""message><button type="submit">Send</button></form></body>')
+        res.write('</head>')
+        return res.end();
+    }
     //process.exit()
+    if (url === '/message' && method === 'POST')
     res.setHeader('Content-Type', 'text/html')
     res.write('<html>')
     res.write('<head><title>my first page</title></head>')
